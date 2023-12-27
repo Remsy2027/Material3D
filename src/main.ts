3aad18248382a6775d7dc2b1b40688a372f0e6c5
Jay patel <jayp962001@gmail.com>
2023-12-27 19:50:49 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -24,10 +24,10 @@ const modelPaths = [
   'https://d2629xvaofl3d3.cloudfront.net/Floor.glb',
   'https://d2629xvaofl3d3.cloudfront.net/Frame.glb',
   'https://d2629xvaofl3d3.cloudfront.net/Plant.glb',
-  'https://d2629xvaofl3d3.cloudfront.net/Window.glb',
   'https://d2629xvaofl3d3.cloudfront.net/Floor_Lamp.glb',
   'https://d2629xvaofl3d3.cloudfront.net/Accessories.glb',
   'https://d2629xvaofl3d3.cloudfront.net/Coffee_Table.glb',
+  'https://d2629xvaofl3d3.cloudfront.net/Window.glb',
   'https://d2629xvaofl3d3.cloudfront.net/Carpet.glb',
   'https://d2629xvaofl3d3.cloudfront.net/Sofa.glb',
   // 'models/Sofa.glb'
